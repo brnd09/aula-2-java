e7726410c56dd5da628c8c289b5fd6d0d3f601b0
Bernardo Mendes <134334489+brnd09@users.noreply.github.com>
2024-03-11 08:41:41 -0300

    Create aula2.java

---
+++ aula2.java
@@ -0,0 +1,35 @@
+package aula_2;
+
+import java.util.Scanner;
+
+public class Aula_2 {
+
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+
+        //Valores inteiros
+        int vr = 10;
+        int ch = 0;
+        int qtdtentativas = 0;
+
+        //Loop de tentativas
+        while ((vr != ch) && (qtdtentativas < 10)) {
+
+            System.out.println("Tenta outra vez: ");
+            ch = teclado.nextInt();
+
+            qtdtentativas = qtdtentativas + 1;
+
+            if ((qtdtentativas >= 2) && (qtdtentativas <= 4));
+            {
+                System.out.println("Dica, chute um numero de 1 a 1000.");
+            }
+            if (ch == vr) {
+                System.out.println("Parabens, voce acertou");
+            }
+
+        }
+
+    }
+}
